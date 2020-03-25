96aa723d95d5626a7504d3103dfdb1a89ecb919f
naterush <naterush1997@gmail.com>
2020-03-25 16:26:00 -0400

    don't serve dist, add headers for cors

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+
+// Add headers to make excel happy
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get('/', function (req, res) {
     res.json({"key": "value"});
