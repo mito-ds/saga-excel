d019f2aaea076518c4b451eefd17644bd2ad5f82
naterush <naterush1997@gmail.com>
2020-03-25 01:52:49 -0400

    server bug fixes

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'taskpane.html'));
+    res.json({"key": "value"});
 });
-console.log(process.env.PORT);
 
-app.listen(process.env.PORT || 8080);
+app.listen(3000);
