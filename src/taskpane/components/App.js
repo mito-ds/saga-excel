fb1651f44dff3fd6dadfae915ea77f83f122f233
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-04-13 23:39:48 -0400

    implemented first taskpane UI

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Header from "./Header";
+import { PrimaryButton } from '@fluentui/react';
 import HeroList from "./HeroList";
 import Progress from "./Progress";
 import CreateButton from "./saga/CreateButton";
@@ -12,6 +13,8 @@ import CheckinButton from "./saga/CheckinButton";
 import CreateFromRemoteForm from './saga/CreateFromRemoteForm'
 import ResetPersonalButton from './saga/ResetPersonalButton'
 //import { updateShared } from "./saga/sync";
+
+import './App.css';
 
 /* global */
 
@@ -48,26 +51,31 @@ export default class App extends React.Component {
 
     if (!isOfficeInitialized) {
       return (
-        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
+        <Progress title={title} logo="assets/saga-logo/saga-logo-taskpane.png" message="Please sideload your addin to see app body." />
       );
     }
     return (
-      <div className="ms-welcome">
-        <Header logo="assets/logo-filled.png" title={this.props.title} message="Saga VCS" />
-        <HeroList message="Welcome to saga." items={[]}> 
-          <p className="ms-font-l">
-            Use the buttons to interact with Saga.
-          </p>
-          <CreateButton/>
-          <CleanupButton/>
-          <CheckinButton/>
-          <CommitForm/>
-          <CreateBranchInput/>
-          <CheckoutBranchInput/>
-          <SeePreviousCommitForm/>
-          <CreateFromRemoteForm/>
-          <ResetPersonalButton/>
-        </HeroList>
+      <div className="taskpane">
+        <div className="content">
+          <div className="header">
+            <img className="saga-logo" src="assets/saga-logo/saga-logo-taskpane.png"/>
+            <p className="title-text">You're almost ready to start collaborating - just tell your team who you are</p>
+          </div>
+          <div className="card-div">          
+            <div className="floating-card">
+              <p className="subtext">I’m using Saga knowing that it is in a pre-alpha stage. I understand that my data may be lost and <b>I will continue to backup my work.</b> </p>
+              <form className="form">
+                <input className="email-input" placeholder="example@gmail.com" type="email"></input>
+                <PrimaryButton className="submit-button">Submit</PrimaryButton>
+              </form>
+            </div>
+          </div>
+          
+        </div>
+        <div className="footer">
+          <p className="FAQ-text"> <b>Have questions about Saga? See our <a href="https://sagalab.org/">FAQ</a></b></p>
+          <p className="subtext disclaimer"> Saga is in pre-alpha stage. Use this tool knowing your data may be lost. </p>
+        </div>
       </div>
     );
   }
