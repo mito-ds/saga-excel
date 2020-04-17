ccb31a7576ca439e90033d3acc25da7e59a0e64a
naterush <naterush1997@gmail.com>
2020-04-17 17:05:19 -0400

    footer cleanup

--- src/taskpane/components/TaskpaneFooter.js
+++ src/taskpane/components/TaskpaneFooter.js
@@ -8,8 +8,8 @@ export default class TaskpaneFooter extends React.Component {
     render() {
         return (
             <div className="footer">
-                <p className="FAQ-text"> <b>Have questions about Saga? See our <a href="https://sagalab.org/">FAQ</a></b></p>
-                <p className="subtext-disclaimer"> Saga is in pre-alpha stage. Use this tool knowing your data may be lost. </p>
+                <p className="FAQ-text"> <b>Have questions about Saga? See our <a href="https://sagalab.org/">FAQ</a> or leave us <a href="https://sagalab.org/contact">feedback</a></b></p>
+                <p className="subtext-disclaimer"> Saga is in pre-alpha stage. Make sure you back up your work! </p>
             </div>
         )
     }
