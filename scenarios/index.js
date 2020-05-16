8e634a6f7d2d59bf2c4d9c22b4baad335c3a4aef
naterush <naterush1997@gmail.com>
2020-05-15 21:59:13 -0400

    add unmerged conflict

--- scenarios/index.js
+++ scenarios/index.js
@@ -1,5 +1,6 @@
 import empty from "./empty.json";
 import referenceAcrossSheets from "./referenceAcrossSheets.json"
+import unmergedConflict from "./unmergedConflict.json"
 
 
-export {empty, referenceAcrossSheets};
+export {empty, referenceAcrossSheets, unmergedConflict};
