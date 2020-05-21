2c7e622bf4bdb063084a2fb28697e996c8320f5d
naterush <naterush1997@gmail.com>
2020-05-21 13:46:33 -0400

    race

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -60,13 +60,13 @@ async function merge(event) {
 }
 
 async function catchUp(event) {
-  console.log("catching up in commands")
-  window.app.setTaskpaneStatus(taskpaneStatus.DIFF);
   const changes = await runCatchUp()
   console.log(changes);
   // We set the diff state as well
   window.app.setChanges(changes);
-  console.log(changes)
+  console.log("catching up in commands")
+  window.app.setTaskpaneStatus(taskpaneStatus.DIFF);
+  
   event.completed();
 }
 
