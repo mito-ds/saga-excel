0ab59ea3b2f00442a0fb037a4a1bb51b4ae42945
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-05-24 12:58:11 -0400

    resolve bug 58

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -8,6 +8,7 @@ import { runResetPersonalVersion } from "../saga/resetPersonal.js";
 import { runMerge } from "../saga/merge.js";
 import { runCatchUp } from "../saga/diff.js";
 import { taskpaneStatus, mergeState } from "../constants";
+import { sagaProjectExists } from "../saga/sagaUtils.js"
 
 /* global global, Office, Excel */
 
@@ -18,15 +19,30 @@ function formattingHandler(event) {
   events.push(event);
 }
 
-async function openShareTaskpane(event) {
-  window.app.setTaskpaneStatus(taskpaneStatus.SHARE);
-  Office.addin.showAsTaskpane();
-  event.completed();
-}
-
 function openMergeTaskpane() {
   window.app.setTaskpaneStatus(taskpaneStatus.MERGE);
   Office.addin.showAsTaskpane();
+}
+
+async function openShareTaskpane(event) {
+  const sagaExists = await isSagaProject();
+  if (sagaExists) {
+    window.app.setTaskpaneStatus(taskpaneStatus.SHARE);
+    Office.addin.showAsTaskpane();
+  }
+  
+  event.completed();
+}
+
+async function isSagaProject() {
+  const projectExists = await sagaProjectExists();
+  if (!projectExists) {
+    window.app.setTaskpaneStatus(taskpaneStatus.CREATE);
+    Office.addin.showAsTaskpane();
+    return false;
+  } 
+
+  return true; 
 }
 
 Office.onReady(() => {
@@ -41,12 +57,16 @@ Office.onReady(() => {
  * @param event {Office.AddinCommands.Event}
  */
 async function merge(event) {
-  openMergeTaskpane();
+  const sagaExists = await isSagaProject();
+  if (sagaExists) {
+    console.log("SAGA PROJECT EXISTS");
+    openMergeTaskpane();
 
-  // update UI and execute merge
-  window.app.setMergeState({status: mergeState.MERGE_IN_PROGRESS, conflicts: null});
-  var mergeResult = await runMerge(events);
-  window.app.setMergeState(mergeResult);
+    // update UI and execute merge
+    window.app.setMergeState({status: mergeState.MERGE_IN_PROGRESS, conflicts: null});
+    var mergeResult = await runMerge(events);
+    window.app.setMergeState(mergeResult);
+  }
 
   // If this function was called by clicking the button, let Excel know it's done
   if (event) {
@@ -57,13 +77,16 @@ async function merge(event) {
 }
 
 async function catchUp(event) {
-  const sheetDiffs = await runCatchUp();
-  console.log("Sheetdiffs", sheetDiffs);
-  // We set the diff state as well
-  window.app.setSheetDiffs(sheetDiffs);
-  console.log("catching up in commands");
-  window.app.setTaskpaneStatus(taskpaneStatus.DIFF);
-  
+  let sheetDiffs = "";
+  const sagaExists = await isSagaProject();
+  if (sagaExists) {
+    sheetDiffs = await runCatchUp();
+
+    // We set the diff state as well
+    window.app.setSheetDiffs(sheetDiffs);
+    window.app.setTaskpaneStatus(taskpaneStatus.DIFF);
+  }
+
   if (event) {
     event.completed();
   }
@@ -71,17 +94,24 @@ async function catchUp(event) {
 }
 
 async function switchVersion(event) {
-  // Todo: render message saying which branch they are on
-  await runSwitchVersionFromRibbon();
-  
+  const sagaExists = await isSagaProject();
+  if (sagaExists) {
+    // Todo: render message saying which branch they are on
+    await runSwitchVersionFromRibbon();
+  }
+
   if (event) {
     event.completed();
   }
 }
 
 async function resetPersonalVersion(event) {
-  // Todo: If on master, tell them they can't
-  await runResetPersonalVersion();
+  const sagaExists = await isSagaProject();
+  if (sagaExists) {
+    // Todo: If on master, tell them they can't
+    await runResetPersonalVersion();
+  }
+
   if (event) {
     event.completed();
   }
