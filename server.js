f524b1eeb7e74a18ae3e26e3558ffda9a9a3b628
naterush <naterush1997@gmail.com>
2020-05-28 20:09:54 -0400

    add back necessary basic server

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+require('./src/models/ProjectSchema');
+require('./src/models/EmailSchema');
+require('./src/models/FeedbackSchema');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+
+/* global process, require */
+
+// Log all our routes
+app.use(logger("short"));
+
+
+// Add headers to make excel happy
+app.use(function(req, res, next) {
+    // TODO: figure out if we need this
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Serve all the assets for the add-in
+app.use(express.static('dist'));
+
+app.listen((process.env.PORT || 3000), () => {console.log(`Server is running on ${process.env.PORT || 3000}`)});
