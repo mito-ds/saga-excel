b85fb7534422fffa67c7f48f810c8407f0fb164f
naterush <naterush1997@gmail.com>
2020-06-06 19:23:59 -0400

    add ability to run a single test

--- src/taskpane/components/DevScreen.js
+++ src/taskpane/components/DevScreen.js
@@ -147,9 +147,23 @@ export default class DevScreen extends React.Component {
     }
 
     render() {
+        // We build an option for each 
         let testSuiteArray = [];
-        Object.keys(testSuites).forEach(function(testSuite) {
-            testSuiteArray.push(<option key={testSuite} value={testSuite}>{testSuite}</option>)
+        Object.keys(testSuites).forEach(function(testSuiteName) {
+            testSuiteArray.push(<option key={testSuiteName} value={testSuiteName}>{testSuiteName}</option>)
+        });
+
+        let testArray = [];
+        Object.keys(testSuites).forEach((testSuiteName) => {
+            let testSuite = testSuites[testSuiteName];
+            Object.keys(testSuite).forEach(testName => {
+                let value = JSON.stringify({
+                    testSuiteName: testSuiteName,
+                    testName: testName
+                });
+
+                testArray.push(<option key={testName} value={value}>{testName}</option>)
+            });
         });
 
 
@@ -171,6 +185,13 @@ export default class DevScreen extends React.Component {
                         <select onChange={async (e) => {await runTestSuite(e.target.value);}}>
                             <option> Select Test Suite</option>
                             {testSuiteArray}                
+                        </select>
+                        <select onChange={async (e) => {
+                                let testObj = JSON.parse(e.target.value);
+                                await runTestSuite(testObj.testSuiteName, testObj.testName);
+                            }}>
+                            <option> Select Indivigual Test</option>
+                            {testArray}                
                         </select>
                     </div>
                     <div className="floating-card" style={devScreenStyle}>
