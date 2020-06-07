b86d05cd92120621dcfedc219728269cbc953913
naterush <naterush1997@gmail.com>
2020-06-07 01:54:02 -0400

    functional component

--- src/taskpane/components/ErrorRevertScreen.js
+++ src/taskpane/components/ErrorRevertScreen.js
@@ -5,33 +5,25 @@ import { runRevertToCommitAndBranch } from "../../saga/sagaUtils";
 import { headerSize, taskpaneStatus } from "../../constants";
 import { PrimaryButton } from '@fluentui/react';
 
-export default class ErrorRevertScreen extends React.Component {
-    constructor(props) {
-        super(props); 
+async function reset(e, safetyCommit, safetyBranch) {
+    e.preventDefault();
+    await runRevertToCommitAndBranch(safetyCommit, safetyBranch);
 
-        this.reset = this.reset.bind(this);
-    }
+    // TODO: Create a success screen or default project management screen
+    window.app.setTaskpaneStatus(taskpaneStatus.SHARE); 
+}
 
-    async reset(e) {
-        e.preventDefault();
-        await runRevertToCommitAndBranch(this.props.safetyCommit, this.props.safetyBranch);
-
-        // TODO: Create a success screen or default project management screen
-        window.app.setTaskpaneStatus(taskpaneStatus.SHARE); 
-    }
-
-    render () {
-        return (
-            <Taskpane header={headerSize.SMALL} title="It looks like you were still editting the spreadsheet">
-                <div className="card-div">
-                    <p> 1. Make sure you're not in cell edittng mode. Hint: clicking on this text should do the trick!</p>
-                    <p> 2. Click on the reset button below. </p>
-                    <p> 3. Try your operation again. </p>
-                    <div className="reset-button-div"> 
-                        <PrimaryButton className="reset-button" type="button" onClick={(e) => this.reset(e)}>Reset</PrimaryButton> 
-                    </div>
+export default function ErrorRevertScreen(props) {
+    return (
+        <Taskpane header={headerSize.SMALL} title="It looks like you were still editting the spreadsheet">
+            <div className="card-div">
+                <p> 1. Make sure you're not in cell edittng mode. Hint: clicking on this text should do the trick!</p>
+                <p> 2. Click on the reset button below. </p>
+                <p> 3. Try your operation again. </p>
+                <div className="reset-button-div"> 
+                    <PrimaryButton className="reset-button" type="button" onClick={(e) => reset(e, props.safetyCommit, props.safetyBranch)}>Reset</PrimaryButton> 
                 </div>
-            </Taskpane>
-        ); 
-    }
+            </div>
+        </Taskpane>
+    ); 
 }
